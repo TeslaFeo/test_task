651aed5b8863edf5d0d87060e3685d8f949b7cd8
Murat_Kataki <murat.kataki@gmail.com>
2023-08-22 21:51:18 +0300

    error messages

--- api/app/Http/Requests/DocumentRequest.php
+++ api/app/Http/Requests/DocumentRequest.php
@@ -16,6 +16,44 @@ class DocumentRequest extends FormRequest
         return true;
     }
 
+    public function attributes(): array
+    {
+        return [
+            'provider' => 'Наименование поставщика',
+            'provider_inn' => 'ИНН поставщика',
+            'provider_kpp' => 'КПП поставщика',
+            'company_logo' => 'Лого компании',
+            'customer_full_name' => 'ФИО покупателя',
+            'customer_inn' => 'ИНН покупателя',
+            'products.*.name' => 'Наименование товара',
+            'products.*.count' => 'Количество',
+            'products.*.price' => 'Сумма',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'required' => 'Поле ":attribute" обязательно для заполнения.',
+            'min' => [
+                'numeric' => 'Поле ":attribute" должно быть не менее :min.',
+                'file' => 'Поле ":attribute" должно быть не менее :min Килобайт.',
+                'string' => 'Поле ":attribute" должно быть не короче :min символов.',
+                'array' => 'Поле ":attribute" должно содержать не менее :min элементов.'
+            ],
+            'max' => [
+                'numeric' => 'Поле ":attribute" должно быть не больше :max.',
+                'file' => 'Поле ":attribute" должно быть не больше :max Килобайт.',
+                'string' => 'Поле ":attribute" должно быть не длиннее :max символов.',
+                'array' => 'Поле ":attribute" должно содержать не более :max элементов.'
+            ],
+            'integer' => 'Поле ":attribute" должно быть целым числом.',
+            'image' => 'Поле ":attribute" должно быть изображением.',
+            'array' => 'Поле ":attribute" должно быть массивом.',
+            'numeric' => 'Поле ":attribute" должно быть числом.',
+        ];
+    }
+
     /**
      * Get the validation rules that apply to the request.
      *
@@ -29,7 +67,7 @@ class DocumentRequest extends FormRequest
             'provider_kpp' => ['required', 'integer', 'min_digits:3', 'max_digits:30'],
             'company_logo' => ['required', 'image', 'max:4096'],
             'customer_full_name' => ['required', 'string', 'min:3', 'max:255'],
-            'customer_inn' => ['required', 'string', 'min:3', 'max:255'],
+            'customer_inn' => ['required', 'integer', 'min_digits:3', 'max_digits:30'],
             'products' => ['required', 'array', 'min:1',],
             'products.*' => ['required', 'array'],
             'products.*.name' => ['required', 'string', 'min:3', 'max:255'],
